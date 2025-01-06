fc904a270341c1d005e3122f828c5aea20256d6e
harry <tl9434@naver.com>
2025-01-06 17:32:41 +0900

    � Set default route to LoginPage on app startup and � Change home page route to /home

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
