d2d8f3477aebe05db2e9a0640d543742279c59ad
harry <tl9434@naver.com>
2025-01-06 19:46:25 +0900

    � feat: Initialize users database

---
+++ src/database/db.jsx
@@ -0,0 +1,17 @@
+export const users = [
+  {
+    email: "user1@example.com",
+    password: "password123",
+    name: "User One",
+  },
+  {
+    email: "user2@example.com",
+    password: "password456",
+    name: "User Two",
+  },
+  {
+    email: "user3@example.com",
+    password: "password789",
+    name: "User Three",
+  },
+];
