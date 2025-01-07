8e5a0b982a8950315693ee2c92227486a892c712
harry <tl9434@naver.com>
2025-01-07 20:00:47 +0900

    ⚡️ 로그아웃 버튼 구현

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,23 +1,55 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
 const TopBar = () => {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogoutClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirmLogout = () => {
+    setOpen(false);
+    navigate('/login');
+  };
+
   return (
-    <AppBar position="sticky" sx={{ minHeight: '64px', width: '100%' }}>
-      <Toolbar>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <InstagramIcon sx={{ marginRight: 1 }} />
-          <Typography variant="h6">
-            SOCIA
-          </Typography>
-        </Box>
-        <Box sx={{ marginLeft: 'auto' }}>
-          <Button color="inherit">
-            Logout
+    <>
+      <AppBar position="sticky" sx={{ minHeight: '64px', width: '100%' }}>
+        <Toolbar>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <InstagramIcon sx={{ marginRight: 1 }} />
+            <Typography variant="h6">SOCIA</Typography>
+          </Box>
+          <Box sx={{ marginLeft: 'auto' }}>
+            <Button color="inherit" onClick={handleLogoutClick}>
+              Logout
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>로그아웃</DialogTitle>
+        <DialogContent>
+          <DialogContentText>로그아웃 하시겠습니까?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" sx={{ fontWeight: '700' }}>
+            취소
           </Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
+          <Button onClick={handleConfirmLogout} color="primary" autoFocus sx={{ fontWeight: '700' }}>
+            로그아웃
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
